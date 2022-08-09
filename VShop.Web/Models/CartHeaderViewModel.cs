63ed71aed78f09adac88df5f52e704d1e0eaf3b0
Gabriel Rodrigues <garodriguesfs10@gmail.com>
2022-08-08 21:41:45 -0300

    add cart api, models, services

---
+++ VShop.Web/Models/CartHeaderViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace VShop.Web.Models;
+
+public class CartHeaderViewModel
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string CouponCode { get; set; } = string.Empty;
+
+    public decimal TotalAmount { get; set; } = 0.00m;
+}
